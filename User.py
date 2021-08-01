06ff41809e22a5acb367ec9429e906ede1174d76
djsfabio <fabio92700@gmail.com>
2021-08-01 17:55:41 +0200

    [FDJ] Projet finalisé.

---
+++ User.py
@@ -0,0 +1,71 @@
+import re
+
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+TELEPHONE_REGEX = re.compile(r"^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$")
+
+class User:
+
+    def __init__(self, name="", company="", email="", telephone="", password=""):
+        self.__name = name
+        self.__company = company
+        self.__email = email
+        self.__telephone = telephone
+        self.__password = password
+
+    # Getters
+    def get_name(self):
+        return self.__name
+
+    def get_company(self):
+        return self.__company
+
+    def get_email(self):
+        return self.__email
+
+    def get_telephone(self):
+        return self.__telephone
+
+    def get_password(self):
+        return self.__password
+
+    # Setters
+    def set_name(self, newName):
+        if len(newName) >= 5:
+            self.__name = newName
+        else :
+            raise Exception("Fill in a correct name and first name (At least 2 characters for the name and the first name separated by a space).")
+            exit()
+
+    def set_company(self, newCompany):
+        if newCompany != "" :
+            if len(newCompany) <= 100 :
+                self.__company = newCompany.capitalize()
+            else :
+                raise Exception("Please enter a company name of less than 100 characters.")
+                exit()
+
+    def set_email(self, newEmail):
+        if newEmail != "":
+            if not EMAIL_REGEX.match(newEmail):
+                raise Exception("Please enter a valid email address.")
+                exit()
+            else:
+                self.__email = newEmail
+
+    def set_telephone(self, newTelephone):
+        if newTelephone != "":
+            if not TELEPHONE_REGEX.match(newTelephone):
+                raise Exception("Please enter a valid French number.")
+                exit()
+            else:
+                self.__telephone = newTelephone
+
+    def set_password(self, newPassword):
+        if newPassword in " " == True:
+            raise Exception("Please provide a valid password without space character.")
+            exit()
+        elif len(newPassword) < 6 :
+            raise Exception("Please provide a valid password without space character and with 6 characters at least.")
+            exit()
+        else:
+            self.__password = newPassword
---
+++ main.py
@@ -0,0 +1,200 @@
+import csv
+import getpass
+import time
+from pathlib import Path
+
+import pyfiglet
+from termcolor import colored
+import os
+
+from Contact import Contact
+from User import User
+
+
+def main():
+    running = True
+    while running:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(colored(pyfiglet.figlet_format("The Business Card Manager", font="slant"), "cyan"))
+        print(colored("1. ", "blue"), "Log In")
+        print(colored("2. ", "green"), "Create New Profile")
+        print(colored("3. ", "red"), "Exit")
+        runningChoice = True
+        while runningChoice:
+            try:
+                userChoice = int(input("\nPlease choose one of the following options : "))
+            except:
+                userChoice = -1
+            if 1 <= userChoice <= 3:
+                runningChoice = False
+            else:
+                print("Please select a value between 1 and 3.")
+
+        if userChoice == 1:
+            login()
+
+        elif userChoice == 2:
+            create()
+
+        elif userChoice == 3:
+            exitProgram()
+
+
+def login():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(colored(pyfiglet.figlet_format("Log In", font="slant"), "green"))
+    notLogIn = True
+    while notLogIn:
+        userId = input("Please enter your login ID : ")
+        userPswd = getpass.getpass('Please enter your password : ')
+        listOfUsers = getListOfUsers()
+        for aUser in listOfUsers:
+            if userId == aUser.get_name() and userPswd == aUser.get_password():
+                print("Welcome back " + userId)
+                actualUser = aUser
+                notLogIn = False
+                break
+        print("The id of connexion or the password you’ve entered is incorrect. Please try again.")
+    loginPage = True
+    while loginPage:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(colored(pyfiglet.figlet_format(userId, font="slant"), "green"))
+        print(colored("1. ", "blue"), "Add a contact")
+        print(colored("2. ", "green"), "See my library")
+        print(colored("3. ", "red"), "Log out")
+        userChoiceLogin = int(input("\nPlease choose one of the following options : "))
+        if userChoiceLogin == 1 :
+            createContact()
+        elif userChoiceLogin == 2 :
+            listOfContacts = getLibrary()
+            for contact in listOfContacts:
+                print(colored("--------------------------------", "green", "on_green"))
+                print("Name :", contact.get_name())
+                print("Company :", contact.get_company())
+                print("E-Mail :", contact.get_email())
+                print("Telephone :", contact.get_telephone())
+            input("To return to the previous page, please press the enter key.")
+        elif userChoiceLogin == 3:
+            loginPage=False
+
+
+
+def create():
+    newUser = User()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(colored(pyfiglet.figlet_format("Create New Profile", font="slant"), "green"))
+    name = input("[Mandatory] Name : ")
+    newUser.set_name(name)
+    company = input("(Optionnal) Company : ")
+    newUser.set_company(company)
+    email = input("(Optionnal) E-Mail : ")
+    newUser.set_email(email)
+    telephone = input("(Optionnal) Telephone : ")
+    newUser.set_telephone(telephone)
+    password = getpass.getpass('[Mandatory] Password : ')
+    newUser.set_password(password)
+
+    print("The profile has been create with the following informations : ")
+    print(colored("--------------------------------", "green", "on_green"))
+    print("Name :", newUser.get_name())
+    print("Company :", newUser.get_company())
+    print("E-Mail :", newUser.get_email())
+    print("Telephone :", newUser.get_telephone())
+    print(colored("--------------------------------", "green", "on_green"))
+
+    addUser(newUser)
+
+    print("Back to the home page...")
+    time.sleep(5)
+
+def createContact():
+    newContact = Contact()
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print(colored(pyfiglet.figlet_format("Add a new contact", font="slant"), "green"))
+    name = input("(Optionnal) Name : ")
+    newContact.set_name(name)
+    company = input("(Optionnal) Company : ")
+    newContact.set_company(company)
+    email = input("[Mandatory] E-Mail : ")
+    newContact.set_email(email)
+    telephone = input("(Optionnal) Telephone : ")
+    newContact.set_telephone(telephone)
+
+    print("The contact has been create with the following informations : ")
+    print(colored("--------------------------------", "green", "on_green"))
+    print("Name :", newContact.get_name())
+    print("Company :", newContact.get_company())
+    print("E-Mail :", newContact.get_email())
+    print("Telephone :", newContact.get_telephone())
+    print(colored("--------------------------------", "green", "on_green"))
+
+    addContact(newContact)
+
+    print("Back to the previous page...")
+    time.sleep(5)
+
+def exitProgram():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(colored("We hope to see you very soon.", "cyan"))
+    exit()
+
+
+def addUser(user):
+    with open('users.csv', 'a+') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [user.get_name(), user.get_company(), user.get_email(), user.get_telephone(), user.get_password()])
+
+def addContact(contact):
+    with open('library.csv', 'a+') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [contact.get_name(), contact.get_company(), contact.get_email(), contact.get_telephone()])
+
+def getLibrary():
+    listOfContacts = []
+    my_file = Path("./library.csv")
+    if my_file.is_file():
+        with open('library.csv', 'r') as fp:
+            reader = csv.reader(fp, delimiter=',')
+
+            for eachRow in reader:
+                newContact = Contact()
+
+                newContact.set_name(eachRow[0])
+                newContact.set_company(eachRow[1])
+                newContact.set_email(eachRow[2])
+                newContact.set_telephone(eachRow[3])
+                listOfContacts.append(newContact)
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(colored("Please create a profile before logging in.", "red"))
+        exit()
+    return listOfContacts
+
+def getListOfUsers():
+    listOfUsers = []
+    my_file = Path("./users.csv")
+    if my_file.is_file():
+        with open('users.csv', 'r') as fp:
+            reader = csv.reader(fp, delimiter=',')
+
+            for eachRow in reader:
+                newUser = User()
+
+                newUser.set_name(eachRow[0])
+                newUser.set_company(eachRow[1])
+                newUser.set_email(eachRow[2])
+                newUser.set_telephone(eachRow[3])
+                newUser.set_password(eachRow[4])
+                listOfUsers.append(newUser)
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(colored("Please create a profile before logging in.", "red"))
+        exit()
+    return listOfUsers
+
+
+if __name__ == '__main__':
+    main()
